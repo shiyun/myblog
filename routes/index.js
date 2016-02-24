aa114bc5656b11c31e06af27fca2a9883cfdf08d
shiyun <shiyun1013@163.com>
2016-02-24 18:15:08 +0800

    2016-02-24

--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,46 @@ router.get('/', function(req, res){
 	res.render('index', {title: '主页', username: req.session.username,success: req.flash('success').toString(),error: req.flash('error').toString()})
 });
 
-router.get('/reg', function(req, res){
-	res.render('reg', {title: '注册', username: req.session.username,success: req.flash('success').toString(),error: req.flash('error').toString()})
-});
 
 router.get('/login', function(req, res){
-	res.render('login', {title: '登录'})
+	res.render('login', {title: '登录', username: req.session.username,success: req.flash('success').toString(),error: req.flash('error').toString()})
 });
 
-router.get('/post', function(req, res){
-	res.render('reg', {title: '发表'})
-});
-
-router.post('/post', function(req, res){
+router.post('/login', function(req, res){
 	var md5 = crypto.createHash('md5'),
 		pwd = md5.update(req.body.password).digest('hex');
-	User.find(req.body.username, function(err){
-	
+
+	Users.find(req.body.username, function(err, userObj){
+		if (!userObj.username){
+			req.flash('error', '用户不存在');
+			return res.redirect('/login');//用户不存在则跳转到登录页
+		}
+		if (userObj.password != pwd){
+			req.flash('error', '密码错误');
+			return res.redirect('/login');//用户不存在则跳转到登录页
+		}
+		req.session.username = req.body.username;
+		res.redirect('/');
 	});
 });
 
-router.get('/logoout', function(req, res){
-	res.render('reg', {title: '注册'})
+router.get('/post', function(req, res){
+	res.render('index', {title: '发表', username: req.session.username,success: req.flash('success').toString(),error: req.flash('error').toString()})
+});
+
+
+
+router.get('/logout', function(req, res){
+	req.session.username = null;
+	req.flash('success', '登出成功!');
+	res.redirect('/');//登出成功后跳转到主页
+});
+
+router.get('/reg', function(req, res){
+	if (req.session.username){
+		return res.redirect('/');
+	}
+	res.render('reg', {title: '注册', username: req.session.username,success: req.flash('success').toString(),error: req.flash('error').toString()})
 });
 
 router.post('/reg', function(req, res){
@@ -61,9 +79,10 @@ router.post('/reg', function(req, res){
 			req.flash('error', err);
 			return res.redirect('/');
 		}
-		
-		if (!obj){
+		if (!!obj){
+		console.log('yes: '+JSON.stringify(obj));
 			if (obj.email){
+				console.log('此邮箱已被注册！');
 				req.flash('error', '此邮箱已被注册！');
 				return res.redirect('/reg');
 				//res.send({'result':{'message': '此邮箱已注册过！'},'status': {'code':'-1','desp':'此邮箱已注册过！'}});
@@ -76,22 +95,14 @@ router.post('/reg', function(req, res){
 				//res.send({'result':{'message': '此邮箱已注册过！'},'status': {'code':'-1','desp':'此邮箱已注册过！'}});
 			}
 
-			Users.save(newUser, function(err){
-				if (err){
-					req.flash('error', err);
-					return res.redirect('/reg');
-				}
-				req.session.username = newUser.username;
-				req.flash('success', '注册成功!');
-				res.redirect('/');//注册成功后返回主页
-			});
 		}else{
+			console.log('no: '+JSON.stringify(obj));
 			Users.save(newUser, function(err){
 				if (err){
 					req.flash('error', err);
 					return res.redirect('/reg');
 				}
-				req.session.username = newUser.username;
+				//req.session.username = newUser.username;
 				req.flash('success', '注册成功!');
 				res.redirect('/');//注册成功后返回主页
 			});
